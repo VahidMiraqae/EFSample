8ae5a2ca932e035291e7286f6c951f2401f53777
Vahid <vahidmiraqae@gmail.com>
2024-07-07 21:44:09 +0330

    sample select

--- EFSample/EntityModels/ChinookContext.cs
+++ EFSample/EntityModels/ChinookContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace EFSample.EntityModels;
 
@@ -36,7 +34,6 @@ public partial class ChinookContext : DbContext
     public virtual DbSet<Track> Tracks { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlite("Data Source=chinook.db;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- EFSample/Program.cs
+++ EFSample/Program.cs
@@ -1,4 +1,12 @@
 ﻿
 
+using EFSample.EntityModels;
+using Microsoft.EntityFrameworkCore;
+
+using var db = new ChinookContext();
+
+var allArtists = db.Artists.Include(a => a.Albums).ThenInclude(a => a.Tracks).AsSplitQuery();
+var query = allArtists.ToQueryString();
+var result = allArtists.ToList();
 
 Console.Read();
